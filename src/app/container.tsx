5443fe1c555fb1774d0e4de6efb66dfe32d442ed
Nestor Turizo <nturizo@rzero.com>
2024-01-05 15:42:52 -0500

    respond to server changes on chart

--- src/app/container.tsx
+++ src/app/container.tsx
@@ -1,19 +1,26 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Chart from './chart';
 import io from 'socket.io-client';
 
 const socket = io('http://localhost:3001');
 
 export default function Container({ votes }) {
+  const [votesRealtime, setVotesRealtime] = useState(votes);
+
   useEffect(() => {
     socket.on('change-event', (message) => {
-      console.log(message);
+      const msg = JSON.parse(message);
+      const { operationType, fullDocument } = msg || {};
+
+      if (operationType === 'insert') {
+        setVotesRealtime((v) => [...v, fullDocument]);
+      }
     });
   }, []);
 
   return (
-    <Chart votes={votes} />
+    <Chart votes={votesRealtime} />
   )
 };
